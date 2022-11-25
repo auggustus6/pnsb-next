5bdc38a8b054c5a34bfaafcc7ed2b30dc6ab0a28
Gustavo Felix <54154635+guusfelix2015@users.noreply.github.com>
2022-11-25 16:35:52 -0300

    feat: responsive footer (#2)

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,5 +1,6 @@
 import Container from "components/Container";
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Wrapper = styled.div`
   padding-bottom: 6rem;
@@ -17,10 +18,20 @@ export const ContainerFooterLogo = styled(Container)`
   display: flex;
   justify-content: space-between;
   align-items: center;
+
+  ${media.lessThan("small")`
+    align-items: flex-start;
+`}
 `;
 
 export const Box = styled.div`
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+`;
+
+export const LogoImage = styled.div`
+  ${media.lessThan("small")`
+  display: none;
+`}
 `;
 
 export const ContentLogo = styled.div`
@@ -28,7 +39,6 @@ export const ContentLogo = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 1rem 0 1rem 0;
-
   gap: 2rem;
 
   h6 {
@@ -46,14 +56,27 @@ export const NavFooter = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
+
+  ${media.lessThan("large")`
+    flex-direction: column;
+    align-items: center;
+    gap: 3rem;
+  `}
 `;
 
 export const WrapperLinks = styled.div`
   display: flex;
-  max-width: 700px;
-  width: 100%;
+  max-width: 850px;
   justify-content: space-between;
+  width: 100%;
   flex-wrap: wrap;
+
+  ${media.lessThan("small")`
+  flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+    justify-content: center;
+  `}
 `;
 
 export const ContainerFooter = styled(Container)`
@@ -61,9 +84,18 @@ export const ContainerFooter = styled(Container)`
   align-items: flex-start;
   flex-direction: column;
   padding-top: 6rem;
+
+  ${media.lessThan("large")`
+    gap: 2rem;
+    align-items: center;
+  `}
 `;
 
-export const ContentInfos = styled.div``;
+export const ContentInfos = styled.div`
+  ${media.lessThan("small")`
+  padding-top: 14px;
+`}
+`;
 
 export const Info = styled.div`
   cursor: pointer;
@@ -83,6 +115,12 @@ export const WrapperContent = styled.div`
   max-width: 480px;
   width: 100%;
   justify-content: space-between;
+
+  ${media.lessThan("small")`
+    flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+  `}
 `;
 
 export const ContentLineText = styled.div`
@@ -109,8 +147,8 @@ export const ContainerItems = styled.div`
 
 export const Social = styled.div`
   margin-top: 30px;
-  margin-top: 1rem;
   cursor: pointer;
+  
   ul {
     display: flex;
     list-style: none;
